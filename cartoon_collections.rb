5c3aacd152420f891c7e68c74b761018a8da4191
Diego Zegarra <diego.zegarra79@icloud.com>
2020-04-28 21:27:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.find {|element| element == valid_calls[i]
+  planeteer_calls.find {|element| element == valid_calls
     puts element
   }
   
